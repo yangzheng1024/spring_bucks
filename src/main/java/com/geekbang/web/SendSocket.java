2ca40231db1564c10fa5757604b29d388d54818e
杨正 <yangzhengjava@yeah.net>
2019-04-04 11:03:50 +0800

    修改websocket导入jar

--- src/main/java/com/geekbang/web/SendSocket.java
+++ src/main/java/com/geekbang/web/SendSocket.java
@@ -3,9 +3,11 @@ package com.geekbang.web;
 import cn.hutool.core.util.StrUtil;
 import com.geekbang.websocket.WebSocketServer;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 
@@ -15,12 +17,12 @@ import java.io.IOException;
 @Controller
 public class SendSocket {
 
-//    @GetMapping("/socket/{cid}")
-//    public ModelAndView socket(@PathVariable String cid) {
-//        ModelAndView mav = new ModelAndView("/socket");
-//        mav.addObject("cid", cid);
-//        return mav;
-//    }
+    @GetMapping("/socket/{cid}")
+    public ModelAndView socket(@PathVariable String cid) {
+        ModelAndView mav = new ModelAndView("/socket");
+        mav.addObject("cid", cid);
+        return mav;
+    }
 
     /**
      * 推送数据接口
