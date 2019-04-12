23412630f6975a2e3be0c488133788867be05d24
杨正 <yangzhengjava@yeah.net>
2019-04-12 14:47:41 +0800

    修改Delay延迟队列demo

--- src/test/java/com/geekbang/delayqueueTest/DelayMain.java
+++ src/test/java/com/geekbang/delayqueueTest/DelayMain.java
@@ -1,9 +1,10 @@
 package com.geekbang.delayqueueTest;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.DelayQueue;
 
@@ -12,18 +13,20 @@ import java.util.concurrent.DelayQueue;
  *
  * @author 杨正
  */
+@Slf4j
 public class DelayMain {
 
     private static DelayQueue<DelayEvent> delayQueue = new DelayQueue<>();
 
     public static void main(String[] args) {
         List<DelayEvent> list = new ArrayList<>();
-        list.add(new DelayEvent("张三", LocalDateTime.now().minus(1, ChronoUnit.MINUTES)));
-        list.add(new DelayEvent("李四", LocalDateTime.now().minus(2, ChronoUnit.MINUTES)));
-        list.add(new DelayEvent("王五", LocalDateTime.now().minus(3, ChronoUnit.MINUTES)));
+        list.add(new DelayEvent("张三", LocalDateTime.now().plus(5, ChronoUnit.SECONDS)));
+        list.add(new DelayEvent("李四", LocalDateTime.now().plus(11, ChronoUnit.SECONDS)));
+        list.add(new DelayEvent("王五", LocalDateTime.now().plus(12, ChronoUnit.SECONDS)));
+        list.forEach(delayEvent -> log.info("队列将要存放的数据：{}", delayEvent));
         /* 放入队列 */
         list.stream()
-                .sorted(Comparator.comparing(DelayEvent::getExcuteTime))
+//                .sorted(Comparator.comparing(DelayEvent::getExcuteTime))
                 .forEach(delayEvent -> delayQueue.offer(delayEvent));
 
         while (true) {
@@ -34,8 +37,8 @@ public class DelayMain {
                 e.printStackTrace();
                 continue;
             }
-            System.out.println(take);
-            System.out.println("------");
+            log.info("取出来的数据：{}", take);
+            log.info("---");
         }
     }
 }
